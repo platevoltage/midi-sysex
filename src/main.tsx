958e8860f53f9cee89e161e8e96c3d6d67ddd621
antieatingactivist <antieatingactivist@gmail.com>
2023-06-23 14:25:09 -0700

    refactored

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#f8d96f",
+        },
+        background: {
+            paper: "#1c252c",
+        },
+    },
+});
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
 );
